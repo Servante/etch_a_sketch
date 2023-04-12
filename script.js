97a9735a11a581c4f9a79c33992ae17edb0205a1
Wesley <matthew282@msn.com>
2023-04-11 22:35:39 -0700

    add css rules and draw logic

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
 
-
 const createGrid = function(x) {
   for(let r = 0; r < x; r++) {
     const row = document.createElement("div");
     row.className = "row";
     for( c = 0; c < x; c++) {
-      const cell = document.createElement("cell");
+      const cell = document.createElement("div");
       cell.className = "cell";
-      cell.innerText = "cell";
+      // cell.innerText = "cell";
+      // cell.back
       row.appendChild(cell);
     }
     container.appendChild(row);
@@ -16,3 +16,19 @@ const createGrid = function(x) {
 }
 
 createGrid(16);
+
+let cells = document.querySelectorAll('.cell');
+
+// let clickEvent = () => {
+//   console.log("hover");
+// }
+
+cells.forEach((cell) => {
+  cell.addEventListener('mousedown', function(event) {
+    event.target.style.backgroundColor = "black";
+  });
+});
+
+
+
+// https://stackoverflow.com/questions/68470917/how-to-draw-via-mouse-click-without-using-the-canvas-element
